4f28b2902e2645d1cc668c65991857a4d3fcedf8
mortetb <mortetb@uio.no>
2024-10-05 18:47:53 +0200

    Added initial code

---
+++ scr/Mainframe.py
@@ -0,0 +1,187 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class MarketOpinionModel:
+    def __init__(self,volitility,mu):
+        """
+        INPUT:
+            volitility: "low", "moderate" and "high"
+            mu_shift: Any value, shifts the median as a percentage of the standard deviation, 
+                      good values are between -1 and 1. Higher values also work.
+        
+        """
+        # To store the probability values
+        self.buychanceList = []
+        self.NobuychanceList = []
+        #volitility parameter should either be "low", "moderate" or "high"
+        if volitility == "low":
+            self.Market_volitility = 0
+        elif volitility == "medium":
+            self.Market_volitility = 1
+        elif volitility == "high":
+            self.Market_volitility = 2
+        else:
+            raise ValueError("Assigned bad value to volitility. Should be strings low, medium or high.")
+        
+        #Pick mean and standard deviation for normal distribution
+        self.sigma = 0.001 *10**(self.Market_volitility)
+        self.mu = mu
+        #To store the values of mu if it updates
+        self.mulist = [self.mu]
+    
+    # Function to set mu as a percentage of sigma
+    def set_mu_by_sigma(self,percentage) -> None:
+        self.mu = percentage*self.sigma
+
+    # Initialises the probability distribution of the investors in the market
+    def Init_MarketOpinion(self,positive) -> None:
+        #Initialises probability values, bound on positive is 0<= positive <= 1
+        if 0>positive or positive >1:
+            raise ValueError("Assigned bad value to positive, should be between 0 or 1")
+        self.buyprob = positive
+        self.nobuyprob = 1-positive
+
+        #Appends values to list
+        self.buychanceList.append(self.buyprob)
+        self.NobuychanceList.append(self.nobuyprob)
+
+    def set_evolver(self,evolver) -> None:
+        if evolver == "G":
+            self.Market_evolver = self.Gaussian_MarketOpinion_evolver
+        elif evolver == "AG":
+            self.Market_evolver = self.Adaptive_Gaussian_MarketOpinion_evolver
+
+    #Updates the probability distribution/opinion of the market 
+    def Gaussian_MarketOpinion_evolver(self,nrcycles=1) -> None:
+
+        #Pick mean and standard deviation for normal distribution
+        mu, sigma =self.mu,self.sigma
+        
+        #Run the cycles
+        if nrcycles == 1:
+            #Picks random variable from normal distribution
+            self.randVar = np.random.normal(mu,sigma) 
+            
+            #If test is to make sure we do not go over 1 or below 0 as this would be non-sensical for a probability
+            if self.buyprob + self.randVar <1 and self.buyprob + self.randVar >0:
+                #Update values
+                self.buyprob += self.randVar
+                self.nobuyprob = 1-self.buyprob
+                #Append values
+                self.buychanceList.append(self.buyprob)
+                self.NobuychanceList.append(self.nobuyprob)
+        elif nrcycles>1:
+            for i in range(nrcycles):
+                #Picks random variable from normal distribution
+                self.randVar = np.random.normal(mu,sigma)
+                if self.buyprob + self.randVar <1 and self.buyprob + self.randVar >0:
+                    #Update values 
+                    self.buyprob += self.randVar
+                    self.nobuyprob = 1-self.buyprob
+                    #Append values
+                    self.buychanceList.append(self.buyprob)
+                    self.NobuychanceList.append(self.nobuyprob)
+    
+    #Should update the mu value
+    def mu_updater(self,regulator = 2) -> None:
+        # We first regulate mu,
+        self.mu = self.mu/regulator
+        #This list contains the percentage of the std that mu is going to move
+        mu_list = [0,0.005,0.01,0.015,0.02,0.025,0.1,0.2]
+        # This list contains the probability of picking and item in mu_list
+        list_prob = [0.50,0.195,0.1,0.1,0.05,0.05,0.0025,0.0025]
+        #Picking the element of mu_list
+        mu_update = np.random.choice(mu_list,p = list_prob)
+        #An if test to reduce numbers of calculations in case 0 is picked
+        if mu_update != 0:
+            # Picking if it is to go up or down
+            updown = np.random.choice([-1,1], p = [0.5,0.5])
+            #Updating mu with a fixed percentage of the standard deviation
+            self.mu += updown * mu_update*self.sigma
+        self.mulist.append(self.mu)
+
+
+
+    def Adaptive_Gaussian_MarketOpinion_evolver(self,nrcycles = 1) -> None:
+        #Run the cycles
+        if nrcycles == 1:
+            #Picks random variable from normal distribution
+            self.randVar = np.random.normal(self.mu,self.sigma) 
+            #If test is to make sure we do not go over 1 or below 0 as this would be non-sensical for a probability
+            if self.buyprob + self.randVar <1 and self.buyprob + self.randVar >0:
+                #Update values
+                self.buyprob += self.randVar
+                self.nobuyprob = 1-self.buyprob
+                #Append values
+                self.buychanceList.append(self.buyprob)
+                self.NobuychanceList.append(self.nobuyprob)
+            self.mu_updater()
+        elif nrcycles>1:
+            for i in range(nrcycles):
+                #Picks random variable from normal distribution
+                self.randVar = np.random.normal(self.mu,self.sigma)
+                if self.buyprob + self.randVar <1 and self.buyprob + self.randVar >0:
+                    #Update values 
+                    self.buyprob += self.randVar
+                    self.nobuyprob = 1-self.buyprob
+                    #Append values
+                    self.buychanceList.append(self.buyprob)
+                    self.NobuychanceList.append(self.nobuyprob)
+                self.mu_updater()
+    def plot_probs(self) -> None:
+        # Just plots the probabilities
+        nrcycles = len(self.buychanceList)
+        time = np.arange(0,nrcycles)
+        plt.grid()
+        plt.plot(time,self.NobuychanceList, label = "NoBuy opinion")
+        plt.plot(time,self.buychanceList, label = "Buy opinion")
+        plt.xlabel("discrete time")
+        plt.ylabel("Probabilities")
+        plt.legend()
+        plt.show() 
+
+    # Plots the evolution of mu as a function of discrete time
+    #Note: Only works for alogirthms that update mu
+    def muplot(self) -> None:
+        nrcycles = len(self.mulist)
+        time = np.arange(0,nrcycles)
+        plt.grid()
+        muarray = np.array(self.mulist)
+        plt.plot(time,muarray / self.sigma)
+        plt.xlabel("discrete time")
+        plt.ylabel(r"$\mu/\sigma$")
+        plt.show()         
+
+
+class StandardStockModel(MarketOpinionModel):
+    def __init__(self,InitValue,volitility,mu):
+        self.Value = InitValue
+        self.ValueList = []
+        super().__init__(volitility,mu)
+
+    def price_time_evolve(self,priceCycles = 1) -> None:
+        self.priceCycles = priceCycles
+        for i in range(priceCycles):
+            #Pick price change
+            self.price_change = np.random.choice([0.01,0.02,0.03,0.04,0.05],p=[0.2,0.3,0.3,0.1,0.1])
+            
+            #Next pick if stock go up or down based on opinion
+            positive = self.buyprob
+            #directionFactor is either +1 of -1 and is based on the market opinion
+            self.directionFactor = np.random.choice([-1,1],p=[1-positive,positive])
+            self.Value += self.directionFactor * self.price_change * self.Value
+            
+            self.ValueList.append(self.Value)
+            self.Market_evolver()
+
+    def plot_stonks(self) -> None:
+        # Plots stock value
+        T = len(self.ValueList)
+        time = np.arange(0,T)
+        plt.grid()
+        plt.plot(time,self.ValueList, label = "Test AS")
+        plt.xlabel("discrete time")
+        plt.ylabel("Stock price")
+        plt.legend()
+        plt.show() 
---
+++ scr/testingGround.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Mainframe import *
+
+cycles = 700
+
+"""
+MarketOpinions = MarketOpinionModel("low",mu= 0)
+
+MarketOpinions.Init_MarketOpinion(0.5)
+
+MarketOpinions.set_evolver("AG")
+
+MarketOpinions.Market_evolver(cycles)
+
+MarketOpinions.muplot()
+MarketOpinions.plot_probs()
+#Opinions.plot_probs()
+"""
+
+
+"""MarketOpinions = MarketOpinionModel("low",mu= 0)
+
+MarketOpinions.Init_MarketOpinion(0.5)
+
+MarketOpinions.Gaussian_MarketOpinion_evolver(cycles)
+
+MarketOpinions.muplot()
+MarketOpinions.plot_probs()
+#Opinions.plot_probs()
+"""
+Stonk = StandardStockModel(200,"low",0)
+Stonk.Init_MarketOpinion(0.5)
+Stonk.set_evolver("G")
+Stonk.price_time_evolve(cycles)
+Stonk.plot_probs()
+Stonk.plot_stonks()
+
+
