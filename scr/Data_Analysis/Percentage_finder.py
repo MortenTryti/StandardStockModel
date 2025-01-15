c786a0353748ee260e106d0e8f4dac3db1e2650f
mortetb <mortetb@uio.no>
2025-01-15 09:45:29 +0100

    changes

--- scr/Data_Analysis/Percentage_finder.py
+++ scr/Data_Analysis/Percentage_finder.py
@@ -9,17 +9,22 @@ for three levels of volatility. The data is gathered from the NASDAQ stocks.
 
 """
 
+# Change this to perform data analysis on selected .txt file
+dataSet_name = "price_open.txt"#"test_data.txt"#
 
+# If this is set to False then the data will not be saved, if set to True then it will be saved 
+Savedata = True
 
-dp = 0.1 # Binsizes
-pUpper = 20 # Upper value of bin
+dp = 0.5e-2 # Binsizes
+pUpper = 1.0 # Upper value of bin
 npoints = int(pUpper/dp) # Number of bins - 1, true number of bins is this and one more truncation bin
+
 
 Lowcounter = np.zeros(npoints+1) # Use this if avg % change is below 2.5
 Lowhisto = []
-Mediumcounter = np.zeros_like(Lowcounter+1) # Use this if avg % change is between 2.5 and 5.0
+Mediumcounter = np.zeros_like(Lowcounter) # Use this if avg % change is between 2.5 and 5.0
 Mediumhisto = []
-Highcounter = np.zeros_like(Lowcounter+1) # Use this if avg % change is above  5.0
+Highcounter = np.zeros_like(Lowcounter) # Use this if avg % change is above  5.0
 Highhisto = []
 
 def percentage_converter(floatList):
@@ -30,11 +35,10 @@ def percentage_converter(floatList):
     """
 
     for i in range(len(floatList)-1):
-        if floatList[i] == 0:
-            p_asset_change = 0
-        else:
+        # If test to root out all the long lines of zero values from the data
+        if floatList[i] != 0 and floatList[i+1] !=0:
             p_asset_change = np.abs(floatList[i+1]/floatList[i]-1)
-        p_change.append(p_asset_change)
+            p_change.append(p_asset_change)
     return p_change 
 
 def historgram_categoriser(number_list, histo_list):
@@ -43,23 +47,25 @@ def historgram_categoriser(number_list, histo_list):
     distribution of price changes for an asset
     
     """
-
+    
     #for loop going through the list of asset percentage change
     for number in number_list:
         # Sorts selected element into the correct bin
         for i in range(npoints):
-            if i*dp <= number <= (i+1)*dp:
+            if i*dp <= number and number < (i+1)*dp:
                 histo_list[i] += 1
-            elif number > pUpper: # This if test sorts into the upper cap truncation bin
-                histo_list[-1] +=1
+                #print(i*dp)
+            elif number >= pUpper: # This if test sorts into the upper cap truncation bin
+                histo_list[npoints] +=1
+
 #TODO make plot of the different stock distributions separatd by volatility
 
 # This is a terrible way to do this, I am so sorry for anyone reading this :)
 
-with open("price_open.txt", "r") as f:
+with open(dataSet_name, "r") as f:
     size=len([0 for _ in f])
     
-with open("price_open.txt", "r") as infile:
+with open(dataSet_name, "r") as infile:
     
     for line in tqdm(infile,total=size, desc="Loading..." ):
         
@@ -91,9 +97,9 @@ with open("price_open.txt", "r") as infile:
 While the three following assets are not used they could be later if we wish to explore the
 true distiributions futher as they contain all the percentage changes for each levels. 
 """
-plotLowHisto = list(np.concatenate(Lowhisto)) 
-plotMediumHisto = list(np.concatenate(Mediumhisto))
-plotHighHisto = list(np.concatenate(Highhisto))
+#plotLowHisto = list(np.concatenate(Lowhisto)) 
+#plotMediumHisto = list(np.concatenate(Mediumhisto))
+#plotHighHisto = list(np.concatenate(Highhisto))
 
 data_bins = [i*dp for i in range(npoints)]+[1e37]
 
@@ -108,7 +114,7 @@ HighVol_dist = np.array(Highcounter)/np.sum(np.array(Highcounter))
 
 #Making the table header
 tableheader = ["Absolute price change (%) ","Low Volatility  $P$" ,"Medium Volatility  $P$", "High Volatility  $P$"]
-tablebins = [f"{i*dp:.2f}-{(i+1)*dp:.2f}" for i in range(npoints)] + [f">{pUpper}.00"]
+tablebins = [f"{i*dp:.2f}-{(i+1)*dp:.2f}" for i in range(npoints)] + [f">{pUpper}"]
 
 #Making the list which can be inserted into the tabulate table
 table_vals_list = []
@@ -118,5 +124,20 @@ for i in range(len(LowVol_dist)):
 print(tabulate(table_vals_list, headers=tableheader, tablefmt='orgtbl'))
 
 
-        
+
+"""
+If Savedata == True the following code saves the probability distributions, the threshold for the
+truncation bin and the binsize.
+"""
+
+if Savedata == True:
+    with open("probability_dist.txt", "w") as infile:
+        infile.write(f"{pUpper} {dp}")
+        infile.write("\n")
+        infile.writelines([str(i)+" " for i in LowVol_dist] )
+        infile.write("\n")
+        infile.writelines([str(i)+" " for i in MedVol_dist])
+        infile.write("\n")
+        infile.writelines([str(i)+" " for i in HighVol_dist])
+            
     
